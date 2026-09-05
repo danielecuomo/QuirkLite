cdcd8fe275757136b8ab7a124d4ea95dd14b2fb5
Daniele Cuomo <cuomo.daniele@outlook.com>
2026-09-05 13:00:52 +0300

    Test wire cut qubit initialization

--- test/circuit/WireCutLogicalRows.test.js
+++ test/circuit/WireCutLogicalRows.test.js
@@ -23,12 +23,24 @@ suite.test("multiQubitGateSkipsCutRow", () => {
     assertThat(circuit.gateAtLocIsDisabledReason(1, 0)).isEqualTo(undefined);
 });
 
-suite.test("multiQubitGateStillNeedsEnoughLiveRows", () => {
+suite.test("multiQubitGateNeedsAndCreatesNewLiveRow", () => {
+    let circuit = new CircuitDefinition(2, [
+        new GateColumn([_, Gates.Special.WireCut]),
+        new GateColumn([Gates.IsingGates.XX, _])
+    ]);
+
+    assertThat(circuit.gateQubitRowsAtColumn(1, 0, Gates.IsingGates.XX)).isEqualTo([0, 2]);
+    assertThat(circuit.minimumRequiredWireCount()).isEqualTo(3);
+    assertThat(circuit.withMinimumWireCount().numWires).isEqualTo(3);
+    assertThat(circuit.gateAtLocIsDisabledReason(1, 0)).isEqualTo(undefined);
+});
+
+suite.test("multiQubitGateCannotStartOnCutRow", () => {
     let circuit = new CircuitDefinition(3, [
         new GateColumn([_, Gates.Special.WireCut, _]),
         new GateColumn([_, Gates.IsingGates.XX, _])
     ]);
 
-    assertThat(circuit.gateQubitRowsAtColumn(1, 1, Gates.IsingGates.XX)).isEqualTo([2]);
+    assertThat(circuit.gateQubitRowsAtColumn(1, 1, Gates.IsingGates.XX)).isEqualTo([]);
     assertThat(circuit.gateAtLocIsDisabledReason(1, 1)).isNotEqualTo(undefined);
 });
