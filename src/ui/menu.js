f55a9164958d8558431e6cfbb625fafeb39637fa
Daniele Cuomo <cuomo.daniele@outlook.com>
2026-09-06 11:25:37 +0300

    Update non-locality anchor href in menu.js

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -21,7 +21,7 @@ function initMenu(revision, obsIsAnyOverlayShowing) {
     const teleportAnchor = /** @type {!HTMLAnchorElement} */ document.getElementById('example-anchor-teleport');
     teleportAnchor.href = '#circuit={%22cols%22:[[1,%22IsingXX%22],[1,1,%22Z^%C2%BD%22],[1,%22Chance2%22],[{%22id%22:%22Ryft%22,%22arg%22:%22pi%20t%22}],[%22Bloch%22],[%22MeasureB%22],[%22|0⟩⟨0|%22,%22|0⟩⟨0|%22,%22Bloch%22],[%22WireCut%22,%22WireCut%22]]}';
     const nonLocalityAnchor = /** @type {!HTMLAnchorElement} */ document.getElementById('example-anchor-non-locality');
-    nonLocalityAnchor.href = '#circuit={%22cols%22:[[1,%22IsingXX%22],[1,%22Amps2%22],[],[%22%3C%3C2%22,1,%22%3E%3E2%22],[1,%22WireCut%22,%22WireCut%22],[%22Amps4%22]]}';
+    nonLocalityAnchor.href = '#circuit={%22cols%22:[[1,%22IsingXX%22],[1,%22Chance2%22],[%22%3C%3C2%22,1,%22%3E%3E2%22],[1,%22WireCut%22,%22WireCut%22],[%22Chance2%22]]}';
 }
 
 export {initMenu, obsMenuIsShowing, closeMenu}
